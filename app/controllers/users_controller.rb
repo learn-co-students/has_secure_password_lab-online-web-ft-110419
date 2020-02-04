c634058763822f713e2e7e330de1e7c892940ddf
Gilblasse <gilblasse@gmail.com>
2020-02-04 10:56:08 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,9 @@ class UsersController < ApplicationController
   end
 
   def create
+    return redirect_to new_user_path unless user_params[:password] == user_params[:password_confirmation]
     @user = User.create(user_params)
+
     session[:user_id] = @user.id
     redirect_to 
   end
@@ -13,7 +15,7 @@ class UsersController < ApplicationController
 
 
   private def user_params
-    params.require(:user).permit(:name, :password)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 
 end
