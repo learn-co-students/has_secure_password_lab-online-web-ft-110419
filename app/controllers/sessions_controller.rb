48670a3bd596d35420ffb27407235f974cd07503
Ivana <ivana_pile@yahoo.com>
2020-04-11 20:56:41 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,22 @@
 class SessionsController < ApplicationController
-    def new
-    end
-  
-    def create
-      user = User.find_by(name: params[:user][:name])
-      authenticated = user.try(:authenticate, params[:user][:password])
-  
-      return redirect_to(controller: 'sessions', action: 'new') unless user
-  
-      session[:user_id] = user.id
-  
-      @user = user
-  
-      redirect_to controller: 'welcome', action: 'home'
-    end
-  
-  
-    def destroy
-      session.delete :user_id
-  
+  def new
+
+  end
+
+  def create
+    @user = User.find_by(:name => params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
       redirect_to '/'
+    else
+      redirect_to :signup
+      flash.alert = "You must be logged in"
+      render 'new'
     end
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to '/'
+  end
 end
--- db/migrate/20200303234314_create_users.rb
+++ db/migrate/20200303234314_create_users.rb
@@ -1,10 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
-    create_table :users do |t|
+    create_table :users, force: :cascade do |t|
       t.string :name
       t.string :password_digest
-
-      t.timestamps
     end
   end
 end
